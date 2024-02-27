6d18d5ae6ba313429b765e8d3607eb45374637c2
Rfn <arfin@hotmail.co.uk>
2024-02-27 01:31:00 +0000

    added inquirer code and removed error handling from employee.js

--- index.js
+++ index.js
@@ -9,13 +9,167 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const Employee = require("./lib/Employee.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const employeeTeam = [];
 
-const createManager =  () => {
+let responses;
 
-    inquirer.createPromptModule([
-        {}
-    ])
+const runApplication = async () => {
+
+    const questions = [
+    {
+        type: `input`,
+        name: `name`,
+        message: `Enter the Manager's name: `
+    },
+    {
+        type: `input`,
+        name: `id`,
+        message: `Enter the Manager's ID: `
+    },
+    {
+        type: `input`,
+        name: `email`,
+        message: `Enter the Manager's email: `
+    },
+    {
+        type: `input`,
+        name: `officeNumber`,
+        message: `Enter the Manager's Office Number: `
+    }
+    ]
+    
+   await inquirer.prompt(questions)
+    .then(answers => {
+        responses = answers;
+        console.log(answers)
+    });
+
+    const manager = new Manager(responses.name, responses.id, responses.email, responses.officeNumber);
+    // console.log(manager)
+    employeeTeam.push(manager);
+    // console.log(employeeTeam);
+    options();
 }
+
+
+const createEngineer = async () => {
+
+    const engQuestions = [
+    {
+        type: `input`,
+        name: `name`,
+        message: `Enter the Engineer's Name: `
+    },
+    {
+        type: `input`,
+        name: `id`,
+        message: `Enter the Engineer's ID: `
+    },  {
+        type: `input`,
+        name: `email`,
+        message: `Enter the Engineer's email: `
+    },
+    {
+        type: `input`,
+        name: `github`,
+        message: `Enter the Engineer's GitHub Username: `
+    }
+]
+
+    await inquirer.prompt(engQuestions)
+        .then(answers => {
+         responses = answers;
+      });
+
+
+
+    const engineer = new Engineer(responses.name, responses.id, responses.email, responses.github)
+      console.log (engineer);
+    employeeTeam.push(engineer);
+
+    options();
+}
+
+
+const createIntern = async () => {
+
+    const intQuestions = [
+    {
+        type: `input`,
+        name: `name`,
+        message: `Enter the Intern's Name: `
+    },
+    {
+        type: `input`,
+        name: `id`,
+        message: `Enter the Intern's ID: `
+    },  {
+        type: `input`,
+        name: `email`,
+        message: `Enter the Intern's email: `
+    },
+    {
+        type: `input`,
+        name: `school`,
+        message: `Enter the Intern's School: `
+    }
+]
+
+    await inquirer.prompt(intQuestions)
+        .then(answers => {
+         responses = answers;
+      });
+
+
+
+    const intern = new Intern(responses.name, responses.id, responses.email, responses.school)
+      console.log (intern);
+    employeeTeam.push(intern);
+
+    options();
+}
+
+
+const options = async () => {
+   
+    const optionQuestions = [
+        {
+            type: `list`,
+            name: `optionList`,
+            message: `Please select one of the options below: `,
+            choices: [`Add an engineer`, `Add an intern`, `Finish building the team`]
+        }]
+        
+   
+        const compile = () => { 
+            if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR, { recursive: true });
+        }  
+      
+          const htmlContent = render(employeeTeam);
+          fs.writeFileSync(outputPath, htmlContent);
+          console.log(`HTML file generated at: ${outputPath}`);
+        
+    };
+        
+    await inquirer.prompt(optionQuestions)
+            .then(answers => {
+                responses = answers;
+            })
+
+    if (responses.optionList === `Add an engineer`){
+        console.log(`logged`)
+        createEngineer();
+    } else if (responses.optionList === `Add an intern`){
+        createIntern();
+    } else if (responses.optionList === `Finish building the team`){
+        compile();
+    }
+
+}
+
+runApplication();
